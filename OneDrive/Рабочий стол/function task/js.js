75f3e03c3ad50b94f3583bad26490530754b2982
Me4hdiyeva <mehdiyevasevda004@gmail.com>
2024-05-14 18:03:07 +0400

    ;,fm;dslmf

---
+++ OneDrive/Рабочий стол/function task/js.js
@@ -0,0 +1,46 @@
+
+              // task1     
+// let h1 = document.querySelector('.ad')
+// function Ad(x,y){
+
+//  return x+y
+// }
+// Ad(" Sevda", "Mehdiyeva")
+// h1.innerHTML = Ad(" Sevda", "Mehdiyeva")
+
+                // task2
+                
+//                 let h1 = document.querySelector('.arr')
+// function arr(a) {
+
+//  let arr = [1,2,3,4,5,6,7,8,9,10]
+//  return arr
+// }
+// arr(a)
+// h1.innerHTML = arr(a)
+
+
+
+  
+
+
+                 // task3
+// let age = prompt('yasinizi daxil edin');
+// if (age > 30){
+//     alert(' senin yasinda olan masin day day masinidir')
+// }
+// else if (age < 30) {
+//     alert('senin yasinda olan masin qenaetcidir ')      
+    
+// } else {
+//     alert('olmusen')
+
+    
+// }
+
+
+
+
+
+
+
